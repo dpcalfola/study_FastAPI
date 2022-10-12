5f3c3416ac820f242442529c1f4e546446cdba16
Fola Flor <dpcalfola@gmail.com>
2022-10-12 21:25:35 +0900

    Feat > Run server with DB
        DB:
            localhost Postgres container
            that is not included this project
    
        FastAPI:
            DB table "books" created by python code
            get/post rest request
    
        pip:
            uvicorn is add to requirements.txt

--- proj_02/main.py
+++ proj_02/main.py
@@ -1,5 +1,50 @@
 from decouple import config
 
+from fastapi import FastAPI, Request
+
+# DB modules
+import databases
+import sqlalchemy
 
 DATABASE_URL = f"postgresql://{config('DB_USER')}:{config('DB_PASSWORD')}@localhost:{config('DB_PORT')}/FastAPI_proj_02"
 
+database = databases.Database(DATABASE_URL)
+metadata = sqlalchemy.MetaData()
+
+# Table
+books = sqlalchemy.Table(
+    "books",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("Title", sqlalchemy.String),
+    sqlalchemy.Column("author", sqlalchemy.String),
+)
+
+engine = sqlalchemy.create_engine(DATABASE_URL)
+metadata.create_all(engine)
+
+app = FastAPI()
+
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
+
+
+@app.get("/books/")
+async def get_all_books():
+    query = books.select()
+    return await database.fetch_all(query)
+
+
+@app.post("/books/")
+async def create_book(request: Request):
+    data = await request.json()
+    query = books.insert().values(**data)
+    last_record_id = await database.execute(query)
+    return {"id": last_record_id}
