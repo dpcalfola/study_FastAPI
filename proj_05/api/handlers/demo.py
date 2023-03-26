37653ae6e5b27272e01e8d5aa43caefde69401d4
Fola Flor <dpcalfola@gmail.com>
2023-03-26 18:38:17 +0900

    Feat > FastAPI projet setup
        1. Dependency
        2. Implement FastAPI
        	get "/"
    	    - docs_url
    	get "/api/v1/demo/"
    	    - Return "Hello FastAPI" json message
        3. Make script file for test
    
        < TASK >
    	3. FastAPI project setup
    
            3.1 Dependency
                3.1.1 requirements.txt
                    fastapi>=0.94.0, <0.95.0
                    uvicorn>=0.20.0, <0.21.0
                    pydantic>=1.9.0, <1.10.0
                3.1.2 requirements.dev.txt
                    <EMPTY NOW>
                3.1.3 requirements.test.txt
                    pytest>=7.1.0, <7.2.0
                    flake8>=5.0.4, < 5.1.0
    
            3.2 Implement FastAPI
                3.2.1 main.py
                    Create FastAPI app
                    run uvicorn server
    
                3.2.2 /api/api.py
                    create_app()
                        Endpoint
                            "/" -> docs
                            "/api/v1/demo" -> handlers/demo.py
                        router setting
    
                3.2.3 /api/responses.py
                    class DetailResponse(BaseModel):
                        -> This class makes response format to json
    
                3.2.4 /api/handlers/
                    -> Practical endpoint code is going to be here
                    3.2.4.1 demo.py
                        router = APIRouter()
                        @router.get("/demo", response_model=DetailResponse)
    
            3.3 Make script file for test
                /test.sh
                    1. Run flake8

---
+++ proj_05/api/handlers/demo.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter
+
+from api.responses.detail import DetailResponse
+
+router = APIRouter(prefix="/api/vi/demo")
+
+
+@router.get("/", response_model=DetailResponse)
+def hello_fast_api():
+    """
+        This is the hello FastAPI endpoint
+    :return:
+        json type
+        {"message: Hello FastAPI"}
+    """
+    return DetailResponse(message="Hello FastAPI")
--- proj_05/main.py
+++ proj_05/main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
+import env
+import uvicorn
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+from api.api import create_app
 
 
-# Press the green button in the gutter to run the script.
+def main():
+    app = create_app()
+    uvicorn.run(app, host=env.HOST, port=env.PORT)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
