c37419493058c0693de15d8edf143b71624797b3
Fola Flor <dpcalfola@gmail.com>
2023-03-26 18:52:34 +0900

    Fix > Correct miss spelling on Endpoint
        /api/vi/demo -> /api/v1/demo
        (vi -> v1)

--- proj_05/api/handlers/demo.py
+++ proj_05/api/handlers/demo.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 
 from api.responses.detail import DetailResponse
 
-router = APIRouter(prefix="/api/vi/demo")
+router = APIRouter(prefix="/api/v1/demo")
 
 
 @router.get("/", response_model=DetailResponse)
