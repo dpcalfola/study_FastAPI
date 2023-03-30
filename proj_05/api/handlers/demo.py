cfcdb08734385fa0d0891def78317585d581d242
Fola Flor <dpcalfola@gmail.com>
2023-03-30 20:55:32 +0900

    Feat > Implement FastAPI demo
        Endpoint (prefix="/api/v1/demo")
            GET "/"
            GET "/state-counter"
            POST "/hello/name-in-body"
            POST "/hello/{name}"
            GET "/hello-query"
    
        < Task >
    
        4. FastAPI demo
    
            4.1 Endpoint (prefix="/api/v1/demo")
    
                4.1.1 GET "/"
                    Return simple string message as json
    
                4.1.2 GET "/state-counter"
                    - Test for when initialize the variable inside of function
                        - Every function calls from request, initialize function variable
                        - But global variable that located outside of function saves state until Exit of App
    
                4.1.3 POST "/hello/name-in-body"
                    - Receive BaseModel class as parameter
                    - Send json in body, response it
    
                4.1.4 POST "/hello/{name}"
                    - Send data with path
    
                4.1.5 GET "/hello-query"
                    - Send data with query
                    - Send data Key-Value as params
                    - Result http address:
                        "/hello-query?name=abcdef"

--- proj_05/api/handlers/demo.py
+++ proj_05/api/handlers/demo.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Query
+from pydantic import BaseModel
 
-from api.responses.detail import DetailResponse
+from api.responses.detail import DetailResponse, DetailStateCounter
 
 router = APIRouter(prefix="/api/v1/demo")
 
@@ -13,4 +14,65 @@ def hello_fast_api():
         json type
         {"message: Hello FastAPI"}
     """
-    return DetailResponse(message="Hello FastAPI")
+
+    return DetailResponse(
+        message=f"Hello FastAPI",
+    )
+
+
+#
+global_state: int = 0
+
+
+@router.get("/state-counter", response_model=DetailStateCounter)
+def state_counter():
+    """
+        Variable counter test
+            global variable: counted
+            functional local variable: not counted
+
+        Global variable counted even if request other place
+
+    :return:message, state, global_state
+    """
+
+    global global_state
+    global_state += 1
+
+    state: int = 0
+    state += 1
+
+    response = DetailStateCounter(
+        message="State counter",
+        state=f"{state}",
+        global_state=f"{global_state}"
+    )
+
+    return response
+
+
+class NameIn(BaseModel):
+    name: str
+    prefix: str = "Mr."
+
+
+@router.post("/hello/name-in-body", response_model=DetailResponse)
+def send_dat_body(name_in: NameIn):
+    """
+        Response with hello name, where name is user provided
+    """
+    return DetailResponse(message=f"hello {name_in.prefix} {name_in.name}")
+
+
+@router.post("/hello/{name}", response_model=DetailResponse)
+def send_data_path(name: str):
+    return DetailResponse(
+        message=f"hello {name}"
+    )
+
+
+@router.get("/hello-query", response_model=DetailResponse)
+def send_data_query(name: str = Query(..., title="Name", description="The name")):
+    return DetailResponse(
+        message=f"Hello {name} - query"
+    )
--- proj_05/api/responses/detail.py
+++ proj_05/api/responses/detail.py
@@ -6,3 +6,9 @@ class DetailResponse(BaseModel):
     DetailResponse represents a response associated with a detailed message
     """
     message: str
+
+
+class DetailStateCounter(BaseModel):
+    message: str
+    state: str
+    global_state: str
