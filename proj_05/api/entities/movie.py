12e6585f8eb7f7983fd101698ce7a2644dc9577c
Fola Flor <dpcalfola@gmail.com>
2023-04-13 20:27:38 +0900

    Feat > Implement movie entity class specifically
    < Task >
    
    7. Implement movie entity class specifically
        7.1 Path: /api/entities/movie.py
        7.2 Make constructor (__init__ method)
            7.2.1 About "*" as parameter
                -> It makes that
                    all parameters can only be passed using their parameter name
            7.2.2 Checking - movie_id should be required
        7.3 Hide each property with underscore and make @property functions
            7.3.1 Protected from modifying without setter method
                - Setter method example:
                        @field_name.setter
                        def field_name(self, new_value):
        7.4 Override __eq__ method
            -> Although they are different object (object reference address are not same)
                If all attribute are same each other,
                these two object consider same

--- proj_05/api/entities/movie.py
+++ proj_05/api/entities/movie.py
@@ -1,6 +1,52 @@
 class Movie:
-    id: str
-    title: str
-    description: str
-    released_year: int
-    watched: bool
+    def __init__(
+        self,
+        *,
+        movie_id: str,
+        title: str,
+        description: str,
+        released_year: int,
+        watched: bool = False
+    ):
+        if movie_id is None:
+            raise ValueError("Movie id is required")
+        self._movie_id = movie_id
+        self._title = title
+        self._description = description
+        self._released_year = released_year
+        self._watched = watched
+
+    @property
+    def movie_id(self) -> str:
+        return self._movie_id
+
+    @property
+    def title(self) -> str:
+        return self._title
+
+    @property
+    def description(self) -> str:
+        return self._description
+
+    @property
+    def released_year(self) -> int:
+        return self._released_year
+
+    @property
+    def watched(self) -> bool:
+        return self._watched
+
+    # Although they are different object,
+    # If all attribute are same each other,
+    # these two object consider same
+    def __eq__(self, o: object) -> bool:
+        if not isinstance(o, Movie):
+            return False
+        if (
+            self.movie_id == o.movie_id
+            and self.title == o.title
+            and self.description == o.description
+            and self.released_year == o.released_year
+            and self.watched == o.watched
+        ):
+            return True
